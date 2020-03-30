cd0ac50aa6f58b534eaec83a037de720b9b85ff5
Ash-JMC <ahall1@jmc.edu.au>
2020-03-30 14:48:05 +1100

    Add empty project + THE CUBE
    - Set up empty Unity project
    - Added TheCube
    - Wrote CubeScript

---
+++ GAM403_A2_FirstnameLastname/Assets/Scripts/CubeScript.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeScript : MonoBehaviour
+{
+    void Start()
+    {
+        print("This is a cube");
+    }
+}
